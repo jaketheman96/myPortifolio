c9bc90681c6300004899e5d3e192f10c4dfe8ded
Jake Chien <jake_chien@outlook.com>
2023-01-13 17:21:46 -0300

    style: add's css on contact section

--- my-portifolio/src/pages/Contact.js
+++ my-portifolio/src/pages/Contact.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import linkedinIcon from '../images/linkedin.png';
 import whatsappIcon from '../images/whatsapp.png';
+import twitchIcon from '../images/twitch.png';
 import '../style/Contact.style.css';
 
 function Contact() {
   const linkedinUrl = 'https://www.linkedin.com/in/jake-chien/';
+  const twitchUrl = 'https://www.twitch.tv/jakechienn';
   const whatsappNum = '(41)99983-7067';
   const TWO_SECONDS = 2000;
 
@@ -12,6 +14,7 @@ function Contact() {
 
   const handleClick = ({ target: { name } }) => {
     const options = {
+      twitch: () => window.open(twitchUrl),
       linkedin: () => window.open(linkedinUrl),
       whatsapp: () => {
         setIsCopied(true);
@@ -29,35 +32,46 @@ function Contact() {
   }, [isCopied]);
 
   return (
-    <div className="contact">
-      <button
-        type="button"
-        onClick={ handleClick }
-      >
-        <img
-          src={ linkedinIcon }
-          alt="linkedin icon"
-          style={ { width: '50px' } }
-          name="linkedin"
-        />
-      </button>
-      <button
-        type="button"
-        onClick={ handleClick }
-      >
-        <img
-          src={ whatsappIcon }
-          alt="whatsapp icon"
-          style={ { width: '50px' } }
-          name="whatsapp"
-        />
-      </button>
-      <div
-        className="copied"
-      >
+    <>
+      <div className="contact">
+        <p>Contact me:</p>
+        <div className="contact-icons">
+          <button
+            type="button"
+            onClick={ handleClick }
+          >
+            <img
+              src={ linkedinIcon }
+              alt="linkedin icon"
+              name="linkedin"
+            />
+          </button>
+          <button
+            type="button"
+            onClick={ handleClick }
+          >
+            <img
+              src={ whatsappIcon }
+              alt="whatsapp icon"
+              name="whatsapp"
+            />
+          </button>
+          <button
+            type="button"
+            onClick={ handleClick }
+          >
+            <img
+              src={ twitchIcon }
+              alt="twitch icon"
+              name="twitch"
+            />
+          </button>
+        </div>
+      </div>
+      <div className="copied">
         {isCopied && <p>Number copied!</p>}
       </div>
-    </div>
+    </>
   );
 }
 
